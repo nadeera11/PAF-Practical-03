6ee3890c368f87309d4e660a448c16e7ecec8253
nadeera11 <anupamamaddumage18503@gmail.com>
2026-02-09 10:04:35 +0530

    Add HTTP endpoints and change server port
    Convert Lab03Application into a REST controller by adding @RestController and two GET mappings ("/" -> "Hello, World!", "/hello" -> "Hello again!"). Also add GetMapping/RestController imports and set server.port=8081 in application.properties to run the app on port 8081.

--- src/main/java/com/example/lab03/Lab03Application.java
+++ src/main/java/com/example/lab03/Lab03Application.java
@@ -2,12 +2,25 @@ package com.example.lab03;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Lab03Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab03Application.class, args);
+	}
+
+	@GetMapping("/")	
+	public String hello() {
+		return "Hello, World!";
+	}
+
+	@GetMapping("/hello")
+	public String helloAgain() {
+		return "Hello again!";
 	}
 
 }
